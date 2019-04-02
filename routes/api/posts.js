521d38369c1bad0ed32fbfa2a3407e04c7bc20b0
sebekos <sebkpl@gmail.com>
2019-04-01 22:22:42 -0500

    Added react and dependancies

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -158,35 +158,28 @@ router.post('/comment/:id', passport.authenticate('jwt', { session: false }), (r
 // @route   DELETE api/post/comment/:id/:comment_id
 // @desc    Delete comment
 // @access  Private
-router.delete(
-    '/comment/:id/:comment_id',
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        Post.findById(req.params.id)
-            .then(post => {
-                // Check to see if comment exists
-                if (
-                    post.comments.filter(
-                        comment => comment._id.toString() === req.params.comment_id
-                    ).length === 0
-                ) {
-                    return res
-                        .status(404)
-                        .json({ commentnotexists: 'Comment does not exist' });
-                }
+router.delete('/comment/:id/:comment_id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            // Check to see if comment exists
+            if (post.comments.filter(comment => comment._id.toString() === req.params.comment_id).length === 0) {
+                return res
+                    .status(404)
+                    .json({ commentnotexists: 'Comment does not exist' });
+            }
 
-                // Get remove index
-                const removeIndex = post.comments
-                    .map(item => item._id.toString())
-                    .indexOf(req.params.comment_id);
+            // Get remove index
+            const removeIndex = post.comments
+                .map(item => item._id.toString())
+                .indexOf(req.params.comment_id);
 
-                // Splice comment out of array
-                post.comments.splice(removeIndex, 1);
+            // Splice comment out of array
+            post.comments.splice(removeIndex, 1);
 
-                post.save().then(post => res.json(post));
-            })
-            .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
-    }
+            post.save().then(post => res.json(post));
+        })
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }));
+}
 );
 
 module.exports = router;
