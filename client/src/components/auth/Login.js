aec9fc1073888e8f4fe81cee1630c645c1581ec1
sebekos <sebkpl@gmail.com>
2019-04-04 22:45:24 -0500

    Added login page

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,10 +1,60 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+    state = {
+        email: '',
+        password: '',
+        errors: {}
+    }
+
+    onChangeHandler = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    onSubmitHandler = (e) => {
+        e.preventDefault();
+
+        const user = {
+            email: this.state.email,
+            password: this.state.password
+        }
+        console.log(user);
+    }
+
     render() {
         return (
-            <div>
-                <h1>Login</h1>
+            <div className="login">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-8 m-auto">
+                            <h1 className="display-4 text-center">Log In</h1>
+                            <p className="lead text-center">Sign in to your DevConnector account</p>
+                            <form onSubmit={(e) => { this.onSubmitHandler(e) }}>
+                                <div className="form-group">
+                                    <input
+                                        type="email"
+                                        className="form-control form-control-lg"
+                                        placeholder="Email Address"
+                                        name="email"
+                                        value={this.state.email}
+                                        onChange={(e) => { this.onChangeHandler(e) }} />
+                                </div>
+                                <div className="form-group">
+                                    <input
+                                        type="password"
+                                        className="form-control form-control-lg"
+                                        placeholder="Password"
+                                        name="password"
+                                        value={this.state.password}
+                                        onChange={(e) => { this.onChangeHandler(e) }} />
+                                </div>
+                                <input type="submit" className="btn btn-info btn-block mt-4" />
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         )
     }
