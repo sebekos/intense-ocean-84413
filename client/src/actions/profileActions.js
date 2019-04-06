580c5640f2b9e2f9ea263c25cab99756a4f217a7
sebekos <sebkpl@gmail.com>
2019-04-06 18:16:46 -0500

    Added profile functionality

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE } from './types';
+import { GET_PROFILE, PROFILE_LOADING, CLEAR_CURRENT_PROFILE, GET_ERRORS } from './types';
 
 //Get current profile
 export const getCurrentProfile = () => dispatch => {
@@ -17,18 +17,31 @@ export const getCurrentProfile = () => dispatch => {
                 type: GET_PROFILE,
                 payload: {}
             }))
-}
+};
 
 //Profile loading
 export const setProfileLoading = () => {
     return {
         type: PROFILE_LOADING
     }
-}
+};
 
 //Clear profile
 export const clearCurrentProfile = () => {
     return {
         type: CLEAR_CURRENT_PROFILE
     }
-}
+};
+
+// Create Profile
+export const createProfile = (profileData, history) => dispatch => {
+    axios
+        .post('/api/profile', profileData)
+        .then(res => history.push('/dashboard'))
+        .catch(err =>
+            dispatch({
+                type: GET_ERRORS,
+                payload: err.response.data
+            })
+        );
+};
