1f253de009a669efced9040eaf773a2cde49e221
sebekos <sebkpl@gmail.com>
2019-04-13 18:16:38 -0500

    Prepared for deploy

---
+++ config/keys_prod.js
@@ -0,0 +1,6 @@
+module.exports = {
+    mongoURI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY,
+    gitClientId: process.env.REACT_APP_CLIENT_ID,
+    gitClientSecret: process.env.REACT_APP_CLIENT_SECRET
+};
